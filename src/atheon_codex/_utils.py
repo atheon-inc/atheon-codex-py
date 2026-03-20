62a059f137a47dff2dc6992861d2265399831c50
Saurabh Ghanekar <ghanekarsaurabh8@gmail.com>
2026-03-19 17:59:46 -0700

    Replace Result class with Ok/Err discriminated union
    - Replace manual model validation with a type-safe discriminated union.
    - Update internal response handlers to return explicit Ok/Err variants.

--- src/atheon_codex/_utils.py
+++ src/atheon_codex/_utils.py
@@ -1,25 +1,34 @@
-from typing import Annotated, Any, Generic, Self, TypeVar
+from enum import StrEnum
+from typing import Any, Generic, Literal, TypeVar
 
-from pydantic import BaseModel, Field, model_validator
+from pydantic import BaseModel, ConfigDict
 
 # TODO: Remove this when minimum supported version becomes >=3.12
 T = TypeVar("T", bound=Any)
 E = TypeVar("E", bound=Any)
 
 
-# TODO: Replace class definition to 'class Result[T: Any, E: Any](BaseModel):' when minimum supported version becomes >=3.12
-class Result(BaseModel, Generic[T, E]):
-    value: Annotated[T | None, Field(default=None)]
-    error: Annotated[E | None, Field(default=None)]
+class ResultStatusEnum(StrEnum):
+    OK = "ok"
+    ERR = "err"
 
-    @model_validator(mode="after")
-    def check_mutual_exclusion_of_value_and_error(self) -> Self:
-        if self.value is None and self.error is None:
-            raise ValueError("Either 'value' or 'error' must be set in a Result.")
 
-        if self.value is not None and self.error is not None:
-            raise ValueError(
-                "Both 'value' and 'error' cannot be set in a Result simultaneously."
-            )
+# TODO: Replace class definition to 'class Ok[T](BaseModel):' when minimum supported version becomes >=3.12
+class Ok(BaseModel, Generic[T]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
-        return self
+    status: Literal[ResultStatusEnum.OK] = ResultStatusEnum.OK
+    value: T
+
+
+# TODO: Replace class definition to 'class Err[E](BaseModel):' when minimum supported version becomes >=3.12
+class Err(BaseModel, Generic[E]):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    status: Literal[ResultStatusEnum.ERR] = ResultStatusEnum.ERR
+    error: E
+
+
+# Discriminated union
+# TODO: Replace type definition to 'type Result[T, E] = Ok[T] | Err[E]' when minimum supported version becomes >=3.12
+Result = Ok[T] | Err[E]
